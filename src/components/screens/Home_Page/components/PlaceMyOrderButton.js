dbe9a16cadcaa1d8ea75b48116f373af875e9941
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-10-09 19:58:07 -0300

    implementado botão de fazer pedido

---
+++ src/components/screens/Home_Page/components/PlaceMyOrderButton.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import placeorder from '../../../../assets/placeorder.gif';
+export default function PlaceMyOrderButton() {
+  return (
+    <PlaceMyOrderContainer>
+      <button className="placeMyOrderButton">
+        <div className="degrade">
+          <p>Fazer meu pedido</p>
+        </div>
+      </button>
+    </PlaceMyOrderContainer>
+  );
+}
+
+const PlaceMyOrderContainer = styled.div`
+  height: 192px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    width: 340px;
+    height: 180px;
+    padding: 0px;
+
+    background-image: url(${placeorder});
+    background-size: cover;
+    border: none;
+    font-size: 20.976px;
+    color: #1c2156;
+    font-family: 'Josefin Slab', serif;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+  }
+  .degrade {
+    position: relative;
+    min-width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0.37);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    p {
+      font-weight: 600;
+      font-size: 40px;
+      color: white;
+    }
+  }
+`;
