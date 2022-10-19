8b5921f0ce2b8923262dfa26b0332174bee361b9
juansued1233536 <92616927+juansued1233536@users.noreply.github.com>
2022-10-19 18:19:05 -0300

    adicionado carousel

--- src/components/screens/Home_Page/components/CardCarouselOrdersAndProducts.js
+++ src/components/screens/Home_Page/components/CardCarouselOrdersAndProducts.js
@@ -1,13 +1,18 @@
 import styled from 'styled-components';
-
-export default function CardCarouselOrdersAndFavoriteds({ image, description, price }) {
+export default function CardCarouselOrdersAndProducts({
+  image,
+  description,
+  price,
+  icon
+}) {
   return (
     <CardOfProduct>
-      <img src={image} alt="" />
+      <img className="banner" src={image} alt="" />
 
       <h1 className="title">{description}</h1>
       <div className="priceProductContainer">
         <p className="priceProduct">R$ {price}</p>
+        <img src={icon} alt="" />
       </div>
     </CardOfProduct>
   );
@@ -24,30 +29,34 @@ const CardOfProduct = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  padding: 0 19px 19px 19px;
+  padding: 0 14px 14px 14px;
   color: white;
   box-shadow: 2px 3px 10px rgba(0, 0, 0, 0.3);
+  padding-top: 0;
 
   .title {
     font-size: 40px;
-    margin-bottom: 20px;
+
     font-weight: 700;
   }
 
   .priceProductContainer {
     width: 100%;
     display: flex;
-    justify-content: start;
+    justify-content: space-between;
     align-items: center;
+
     p {
       font-size: 20px;
       font-weight: 600;
     }
   }
 
-  img {
-    max-width: 100%;
-    max-width: 175px;
+  .banner {
+    margin-top: -1px;
+    min-width: 100%;
+    max-width: 173px;
+
     border-radius: 10px 10px 59px 59px;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   }
